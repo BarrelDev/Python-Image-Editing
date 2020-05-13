966c16509480ff34ba84318ee1bf45ab23f48888
AbsorbedBarrel <47505192+AbsorbedBarrel@users.noreply.github.com>
2020-05-12 22:06:47 -0400

    added files

---
+++ image.py
@@ -0,0 +1,27 @@
+from PIL import Image, ImageFilter, ImageEnhance
+
+contrastDelta = input("Please enter a value for contrast -> ")
+widthMultiplier = input("Please enter a width multiplier -> ")
+lengthMultiplier = input("Please enter a length multiplier -> ")
+imagePath = input("Please enter an image path -> ")
+
+image = Image.open(imagePath)  
+print("Image loaded !!")
+
+image.show()
+print(image.size)
+
+enh=ImageEnhance.Contrast(image)
+enh.enhance(int(contrastDelta)).show()
+enh.enhance(int(contrastDelta)).save('contrast.png')
+
+#Create and Save enlarged image
+size_tuple=image.size
+print(size_tuple)
+sizelist=list(size_tuple)
+newwidth=sizelist[0]*int(widthMultiplier)
+newheight=sizelist[1]*int(lengthMultiplier)
+newsize=(newwidth,newheight)
+img_resized=image.resize(newsize)
+img_resized.show()
+img_resized.save('enlarge.png')
